17d44020694bec970e48b7a69dbd372d46f7631d
linszay <lindseyjlancaster@gmail.com>
2023-12-04 06:36:53 -0600

    fixed issues with table creation

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -2,7 +2,7 @@
 -- changing things up by adding more tables
 -- moving around existing tables as well
 -- episodes
-CREATE TABLE EpisodeDates (
+CREATE TABLE IF NOT EXISTS EpisodeDates (
     EPISODE_ID INT PRIMARY KEY,
     TITLE VARCHAR(255),
     ORIGINAL_BROADCAST_DATE DATE
@@ -10,38 +10,29 @@ CREATE TABLE EpisodeDates (
 );
 
 -- subject matters
-CREATE TABLE SubjectMatter (
+CREATE TABLE IF NOT EXISTS SubjectMatter (
     SUBJECT_ID INT PRIMARY KEY,
     NAME VARCHAR(255) UNIQUE NOT NULL
 );
 
--- new table for episode and subject matter relationship
-CREATE TABLE EpisodeSubjectMatter (
-    EPISODE_ID INT,
-    SUBJECT_ID INT,
-    PRIMARY KEY (EPISODE_ID, SUBJECT_ID),
-    FOREIGN KEY (EPISODE_ID) REFERENCES Episode(EPISODE_ID),
-    FOREIGN KEY (SUBJECT_ID) REFERENCES SubjectMatter(SUBJECT_ID)
-);
-
 -- colors
-CREATE TABLE ColorPallete (
+CREATE TABLE IF NOT EXISTS ColorPallete (
     COLOR_ID INT PRIMARY KEY,
     NAME VARCHAR(255) UNIQUE NOT NULL
     -- not sure if i need to add more here yet
 );
 
--- new table for painting and color relationship
-CREATE TABLE PaintingColor (
-    PAINTING_ID INT,
-    COLOR_ID INT,
-    PRIMARY KEY (PAINTING_ID, COLOR_ID),
-    FOREIGN KEY (PAINTING_ID) REFERENCES Painting(PAINTING_ID),
-    FOREIGN KEY (COLOR_ID) REFERENCES Color(COLOR_ID)
+-- new table for episode and subject matter relationship
+CREATE TABLE IF NOT EXISTS EpisodeSubjectMatter (
+    EPISODE_ID INT,
+    SUBJECT_ID INT,
+    PRIMARY KEY (EPISODE_ID, SUBJECT_ID),
+    FOREIGN KEY (EPISODE_ID) REFERENCES EpisodeDates(EPISODE_ID),
+    FOREIGN KEY (SUBJECT_ID) REFERENCES SubjectMatter(SUBJECT_ID)
 );
 
 -- paintings
-CREATE TABLE Painting (
+CREATE TABLE IF NOT EXISTS Painting (
     PAINTING_ID INT PRIMARY KEY,
     EPISODE_ID INT,
     img_src VARCHAR(255),
@@ -50,5 +41,14 @@ CREATE TABLE Painting (
     num_colors INT,
     youtube_src VARCHAR(255),
     -- might need to add more here idk yet
-    FOREIGN KEY (EPISODE_ID) REFERENCES Episode(EPISODE_ID)
+    FOREIGN KEY (EPISODE_ID) REFERENCES EpisodeDates(EPISODE_ID)
+);
+
+-- new table for painting and color relationship
+CREATE TABLE IF NOT EXISTS PaintingColor (
+    PAINTING_ID INT,
+    COLOR_ID INT,
+    PRIMARY KEY (PAINTING_ID, COLOR_ID),
+    FOREIGN KEY (PAINTING_ID) REFERENCES Painting(PAINTING_ID),
+    FOREIGN KEY (COLOR_ID) REFERENCES ColorPallete(COLOR_ID)
 );
